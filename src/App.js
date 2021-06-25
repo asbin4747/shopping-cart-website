8fdade367771dfacc1231742fd5915d8f600c5e1
asbin4747 <asbin.dahal4747@gmail.com>
2021-06-25 14:36:17 -0500

    Uploading files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header >
+       <a href="/" >React Shopping Cart</a>
+      </header>
+
+      <main>
+        Product List
+      </main>
+
+      <footer>
+        All rights reserved
+      </footer>
+    </div>
+  );
+}
+
+export default App;
