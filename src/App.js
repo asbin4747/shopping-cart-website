49cf1d250affae460102fa3c913779cf354a225f
asbin4747 <asbin.dahal4747@gmail.com>
2021-06-28 13:40:10 -0500

    Added products and prices

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import './App.css';
+import React from "react";
+import data from "./data.json";
+import Products from "./components/Products";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header >
-       <a href="/" >React Shopping Cart</a>
-      </header>
 
-      <main>
-        Product List
-      </main>
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    };
+  }  
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
 
-      <footer>
-        All rights reserved
-      </footer>
-    </div>
-  );
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products ={this.state.products} />
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+
+        <footer>All rights reserved</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
